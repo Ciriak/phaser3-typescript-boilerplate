0574ef3ca06afe89b76bd21895e55734df747205
Cyriaque <cyriaquedelaunay@gmail.com>
2020-09-29 17:56:13 +0200

    Updated dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,54 @@
-const path = require('path');
-var LiveReloadPlugin = require('webpack-livereload-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
-var webpack = require('webpack')
+const path = require("path");
+var LiveReloadPlugin = require("webpack-livereload-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
+var webpack = require("webpack");
 
 var definePlugin = new webpack.DefinePlugin({
-    __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || 'true')),
-    WEBGL_RENDERER: true,
-    CANVAS_RENDERER: true
-})
+  __DEV__: JSON.stringify(JSON.parse(process.env.BUILD_DEV || "true")),
+  WEBGL_RENDERER: true,
+  CANVAS_RENDERER: true,
+});
 
 module.exports = {
-    entry: './src/app.ts',
-    mode: 'development',
-    target: 'web',
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: 'ts-loader',
-                exclude: /node_modules/
-            },
-            {
-                test: /\.scss$/,
-                use: [
-                    "style-loader", // creates style nodes from JS strings
-                    "css-loader", // translates CSS into CommonJS
-                    "sass-loader" // compiles Sass to CSS, using Node Sass by default
-                ]
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js']
-    },
-    output: {
-        filename: 'game.js',
-        path: path.resolve(__dirname, 'dist')
-    },
+  entry: "./src/app.ts",
+  mode: "development",
+  target: "web",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", // creates style nodes from JS strings
+          "css-loader", // translates CSS into CommonJS
+          "sass-loader", // compiles Sass to CSS, using Node Sass by default
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  output: {
+    filename: "game.js",
+    path: path.resolve(__dirname, "dist"),
+  },
 
-    plugins: [
-        definePlugin,
-        new LiveReloadPlugin(),
-        new CopyPlugin([
-            { from: 'src/index.html' },
-            {
-                from: 'src/assets',
-                to: 'assets'
-            }
-        ]),
-    ]
+  plugins: [
+    definePlugin,
+    new LiveReloadPlugin(),
+    new CopyPlugin({
+      patterns: [
+        { from: "src/index.html" },
+        {
+          from: "src/assets",
+          to: "assets",
+        },
+      ],
+    }),
+  ],
 };
